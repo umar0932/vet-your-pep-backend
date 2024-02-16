28b55a53300a143aa496b4a91d9a4b5bdf11d840
umar0932 <umar.khalid@chirptech.net>
2024-02-17 04:44:53 +0500

    base entity fixed

--- src/modules/common/entities/base.entity.ts
+++ src/modules/common/entities/base.entity.ts
@@ -3,8 +3,8 @@ import { Column, CreateDateColumn, UpdateDateColumn } from 'typeorm'
 @ObjectType()
 export abstract class CustomBaseEntity {
   @Column({ length: 50, type: 'varchar', default: 'system', name: 'created_by' })
-  @Field()
-  createdBy: string
+  @Field(() => String, { nullable: true })
+  createdBy?: string
 
   @Column({
     length: 50,
@@ -12,14 +12,14 @@ export abstract class CustomBaseEntity {
     default: 'system',
     name: 'updated_by'
   })
-  @Field()
+  @Field(() => String, { nullable: true })
   updatedBy?: string
 
   @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP', name: 'created_at' })
-  @Field()
-  createdDate: Date
+  @Field({ nullable: true })
+  createdDate?: Date
 
   @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP', name: 'updated_at' })
-  @Field()
-  updatedDate: Date
+  @Field({ nullable: true })
+  updatedDate?: Date
 }
